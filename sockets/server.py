df09caf66befbcf45448ef8ced232a3874155953
ardus-uk <peter@ardus.co.uk>
2021-07-25 20:44:04 +0100

    Move server port to json file

--- sockets/server.py
+++ sockets/server.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import socket
+import json
+import os
+
 from datetime import datetime
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+jcf = os.path.join(script_directory, "config_server.json")
+with open(jcf) as json_credentials_file:
+    credentials = json.load(json_credentials_file)
+
+# Choose a server - could be u3apeter, dxantix, or markpi
+server = 'u3apeter'
+
 # Listen from any host
 HOST = ''
 # Listen on the following port
-PORT = 4472
+PORT = credentials['port']
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
